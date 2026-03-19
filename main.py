86244f319ddce874778ad835ed25e59ec32494e5
ABdroid <aadhirai.b@babajividhyashram.org>
2026-03-19 15:13:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ os.environ["GOOGLE_API_KEY"] = api_key
 
 st.set_page_config(page_title="What's For Dinner", page_icon="🥪", layout="wide")
 st.markdown("<h1 style='text-align: center; margin-top: -50px;'>What's For Dinner 🥪</h1>", unsafe_allow_html=True)
-st.write("- a recipe recommender")
 st.write()
 st.set_page_config(layout="wide") 
 
