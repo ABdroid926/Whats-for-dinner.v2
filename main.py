9e6a53aa56cd5d8eec5542880a787da7c3a33a80
ABdroid <aadhirai.b@babajividhyashram.org>
2026-03-19 11:19:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import os
+
+from PIL import Image
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model = genai.GenerativeModel('gemini-pro-vision')
+
+def get_gemini_response(input, image, prompt):
+    response = model.generate_content([input, image[0], prompt])
+    return response.text
+
+def input_image_details(uploaded_file):
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,
+                "data": bytes_data
+            }
+        ]
+
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+st.set_page_config(page_title="Food Scan")
+
+st.header('Food Scan with Google Gemini')
+input = st.text_input("Input prompt: ", key='input')
+uploaded_file = st.file_uploader("Choose an image of the food or food table", type=["jpg", 'jpeg', 'png'])
+image = ""
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+submit = st.button("Scan the Food(s)")
