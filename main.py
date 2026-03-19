b52ae41be25b1ea01e5519d700c827b0f555e819
ABdroid <aadhirai.b@babajividhyashram.org>
2026-03-19 17:53:20 +0530

    Insert empty write statements in main.py
    Add multiple empty write statements for spacing.

--- main.py
+++ main.py
@@ -16,7 +16,17 @@ os.environ["GOOGLE_API_KEY"] = api_key
 
 st.set_page_config(page_title="What's For Dinner?", page_icon="🥪", layout="wide")
 st.markdown("<h1 style='text-align: center; margin-top: -50px;'>What's For Dinner? 🥪</h1>", unsafe_allow_html=True)
-st.container(10)
+
+st.write()
+st.write()
+st.write()
+st.write()
+st.write()
+st.write()
+st.write()
+st.write()
+st.write()
+st.write()
 
 st.divider()
 
